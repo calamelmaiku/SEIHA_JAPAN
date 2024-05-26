941ab19be008d418a5d15fc3162cf8049f6a515e
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-26 23:44:30 +0900

    達成率の表示(グループ詳細ページ)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,5 +8,14 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
     @spots = @group.spots
+
+     # グループの全スポット数
+     total_spots = @spots.count
+
+     # 訪問済みスポット数
+     visited_spots = @spots.joins(:visiteds).where(visiteds: { visited: true, user_id: current_user.id }).count
+ 
+     # 達成率の計算（スポットが0の場合は0%）
+     @completion_rate = total_spots > 0 ? (visited_spots.to_f / total_spots * 100).round(2) : 0
   end
 end
