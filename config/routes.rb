cc6930658042eba65a28d1fbe88911e4a960d29a
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-26 22:38:23 +0900

    ルートコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
   resources :groups, only: [:index, :show] do
-    resources :spots, only: [:show]
+    resources :spots do
+      # スポットの訪問状態をトグルするルートを追加
+      patch :toggle_visited, on: :member
+    end
   end
   resources :users, only: [:edit, :update]
 end
