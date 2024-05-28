f05f55ac582719289da18e80832f7c3c5734f0ba
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-28 14:53:14 +0900

    シードファイル変更

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,19 +1,29 @@
+// DOMContentLoadedイベントが発生した時点で実行される関数を設定
 document.addEventListener("DOMContentLoaded", function() {
   let map;
 
+  // Google Mapsを初期化する関数
   function initMap() {
+    // ジオコーダーの初期化
     const geocoder = new google.maps.Geocoder();
 
+    // Google Mapオブジェクトの作成
     map = new google.maps.Map(document.getElementById('map'), {
+      // 地図の中心を設定（緯度と経度）
       center: { lat: 40.7828, lng: -73.9653 },
+      // ズームレベルを設定
       zoom: 12,
     });
 
+    // マーカーの作成
     const marker = new google.maps.Marker({
+      // マーカーを配置する位置を設定（緯度と経度）
       position: { lat: 40.7828, lng: -73.9653 },
+      // マーカーを表示する地図を設定
       map: map
     });
   }
 
-  window.initMap = initMap; // グローバルスコープにinitMap関数を設定
+  // グローバルスコープにinitMap関数を設定（Google Maps APIのコールバックで使用するため）
+  window.initMap = initMap;
 });
