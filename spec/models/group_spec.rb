c9d737426cc8459778d3c1abdf17f8b9f09e7b0c
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-29 14:51:35 +0900

    グループモデルテストコード

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @group = FactoryBot.build(:group)
+  end
+
+  describe 'グループ新規作成' do
+    context '新規作成できるとき' do
+      it 'name、creator、textが存在すれば作成できる' do
+        expect(@group).to be_valid
+      end
+    end
+
+    context '新規作成できないとき' do
+      it 'nameが空では作成できない' do
+        @group.name = ''
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'creatorが空では作成できない' do
+        @group.creator = ''
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Creator can't be blank")
+      end
+
+      it 'textが空では作成できない' do
+        @group.text = ''
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Text can't be blank")
+      end
+
+      it 'nameが重複している場合は作成できない' do
+        @group.save
+        duplicate_group = @group.dup
+        duplicate_group.valid?
+        expect(duplicate_group.errors.full_messages).to include("Name has already been taken")
+      end
+    end
+  end
 end
+
+
