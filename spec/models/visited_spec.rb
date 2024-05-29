624b989bde8866f5e32ed778cdd90ecaf9f55119
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-29 12:02:12 +0900

    訪れたモデルテストコード

--- spec/models/visited_spec.rb
+++ spec/models/visited_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Visited, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @visited = FactoryBot.build(:visited)
+  end
+
+  describe '訪問記録の作成' do
+    context '訪問記録が有効な場合' do
+      it 'userとspotとvisitedがあれば登録できる' do
+        expect(@visited).to be_valid
+      end
+    end
+
+    context '訪問記録が無効な場合' do
+      it 'userが無ければ登録できない' do
+        @visited.user = nil
+        expect(@visited).not_to be_valid
+      end
+
+      it 'spotが無ければ登録できない' do
+        @visited.spot = nil
+        expect(@visited).not_to be_valid
+      end
+
+      it 'visitedがnilでも登録できる（デフォルト値がある）' do
+        @visited.visited = nil
+        expect(@visited).to be_valid
+      end
+    end
+
+    context 'アソシエーションのテスト' do
+      it 'ユーザーとの関連付けがある' do
+        assoc = described_class.reflect_on_association(:user)
+        expect(assoc.macro).to eq :belongs_to
+      end
+
+      it 'スポットとの関連付けがある' do
+        assoc = described_class.reflect_on_association(:spot)
+        expect(assoc.macro).to eq :belongs_to
+      end
+    end
+  end
 end
