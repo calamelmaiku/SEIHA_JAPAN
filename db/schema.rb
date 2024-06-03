c29edf11e64062b65c0297d055a266c55fcae38d
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-06-03 11:33:43 +0900

    管理者カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_26_112844) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_03_021630) do
   create_table "groups", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "creator"
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_26_112844) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
   end
 
   create_table "visiteds", charset: "utf8", force: :cascade do |t|
