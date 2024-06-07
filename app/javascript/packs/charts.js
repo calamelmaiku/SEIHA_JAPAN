5b4ddfa27737b12496545664e7248425caf8292f
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-06-07 12:45:33 +0900

    円グラフグラデーション調整

--- app/javascript/packs/charts.js
+++ app/javascript/packs/charts.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
   const charts = document.querySelectorAll('.group-completion-chart');
 
   charts.forEach(chart => {// 各グラフ要素の処理
-    const completionRate = parseFloat(chart.getAttribute('data-completion-rate')); // data属性取得し、数値に変換
-    const groupName = chart.getAttribute('data-group-name'); // グループ名をdata属性から取得
-    const ctx = chart.querySelector('canvas').getContext('2d'); // canvas要素のコンテキストを取得
-    new Chart(ctx, { // 新しいChartインスタンスを作成
+    const completionRate = parseFloat(chart.getAttribute('data-completion-rate'));// data属性取得し、数値に変換
+    const groupName = chart.getAttribute('data-group-name');// グループ名をdata属性から取得
+    const color = getGradientColor(completionRate);// getGradientColor関数で色を決定
+    const ctx = chart.querySelector('canvas').getContext('2d');// canvas要素のコンテキストを取得
+    new Chart(ctx, {// 新しいChartインスタンスを作成
       type: 'doughnut', // ドーナツグラフを指定
       data: {
         datasets: [{
@@ -16,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
       },
       options: {
         responsive: true, // グラフをレスポンシブに
+        cutout: '70%', // ドーナツチャートの内側の穴のサイズを指定
         plugins: {
           legend: {
             display: false // 凡例を表示しない
@@ -41,8 +43,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // 完了率に応じた色を取得する関数
   function getGradientColor(percentage) {
-    const red = Math.min(255, Math.floor((1 - percentage / 100) * 255));
-    const green = Math.min(255, Math.floor((percentage / 100) * 255));
-    return `rgb(${red}, ${green}, 0)`;
+    const baseRed = 255;
+    const baseGreen = 255;
+    const baseBlue = 255;
+    const targetRed = 47;
+    const targetGreen = 79;
+    const targetBlue = 79;
+
+    const red = Math.floor(baseRed + (targetRed - baseRed) * (percentage / 100));
+    const green = Math.floor(baseGreen + (targetGreen - baseGreen) * (percentage / 100));
+    const blue = Math.floor(baseBlue + (targetBlue - baseBlue) * (percentage / 100));
+
+    return `rgb(${red}, ${green}, ${blue})`;
   }
 });
