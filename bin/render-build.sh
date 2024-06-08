45003c8d16b1cbffb0b99147f54b3b4ec6247836
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-06-08 13:11:38 +0900

    binファイル修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,3 +9,5 @@ bundle exec rake assets:clean
 
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 bundle exec rake db:seed
+
+RAILS_ENV=production bundle exec rails assets:precompile
