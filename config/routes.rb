dac8f37552fe77231170912886d2c79cd3984ef9
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-07-01 19:09:30 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
+
   resources :groups, only: [:index, :show] do
     resources :spots, only: [:show] do
       # スポットの訪問状態をトグルするルートを追加
       patch :toggle_visited, on: :member
     end
   end
-  resources :users, only: [:show, :edit, :update]
+
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get :following, :followers
+    end
+  end
+  
+  resources :relationships, only: [:create, :destroy]
 
   # admin という名前空間を作成 全てURLの先頭に /admin がつく
   namespace :admin do
