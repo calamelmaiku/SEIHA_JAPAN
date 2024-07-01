45347501701e2f7f95d2017517ed0b74229fa84c
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-07-01 21:09:21 +0900

    自分自身をフォローできない

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,12 @@
 class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    if user != current_user
+      current_user.follow(user)
+      redirect_to user
+    else
+      redirect_to user, alert: "自分自身をフォローすることはできません。"
+    end
   end
 
   def destroy
